2d379f1a5dd09e73585a4a657e157db21378b304
Nik <nerdvestor@gmail.com>
2018-01-17 12:08:06 -0800

    add dragable fronts-end to create playlist

--- app/assets/javascripts/episode.js
+++ app/assets/javascripts/episode.js
@@ -3,3 +3,25 @@ document.addEventListener('turbolinks:load', function () {
       $(".episode-data").toggle();
   });
 })
+
+
+  interact('.episode-drag')
+    .draggable({
+      onmove: window.dragMoveListener
+    })
+
+  function dragMoveListener (event) {
+      var target = event.target,
+          // keep the dragged position in the data-x/data-y attributes
+          x = (parseFloat(target.getAttribute('data-x')) || 0) + event.dx,
+          y = (parseFloat(target.getAttribute('data-y')) || 0) + event.dy;
+
+      // translate the element
+      target.style.webkitTransform =
+      target.style.transform =
+        'translate(' + x + 'px, ' + y + 'px)';
+
+      // update the posiion attributes
+      target.setAttribute('data-x', x);
+      target.setAttribute('data-y', y);
+    }
