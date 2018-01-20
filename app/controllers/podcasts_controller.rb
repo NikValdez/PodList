80fc7e2fe080d85c5e69a22dc932d8a20f599c28
Nik <nerdvestor@gmail.com>
2018-01-20 13:46:05 -0800

    add button styling

--- app/controllers/podcasts_controller.rb
+++ app/controllers/podcasts_controller.rb
@@ -1,13 +1,13 @@
 class PodcastsController < ApplicationController
   def search
-    @podcasts = Search.pod_feed
-
-
     # Avoid requesting info from API if there was no search query
     term = params[:term]
     return unless term.present?
 
-
-      @results = ITunesSearchAPI.search(term: "#{term}", country: "US", media: "podcast")
+    @results = ITunesSearchAPI.search(term: "#{term}", country: "US", media: "podcast")
+      respond_to do |format|
+      format.js
+      format.html
+    end
   end
 end
